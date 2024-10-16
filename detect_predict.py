03a203e03e1eaef84df7c9ce44f4736f39ac5ffb
candi4 <hojun327@naver.com>
2024-10-16 17:38:06 +0900

    Revised predict code to use only related to bounding box

--- detect_predict.py
+++ detect_predict.py
@@ -10,10 +10,6 @@ results = model('/home/hojun/project/YOLO/ezgif-6-13eb75ace9-jpg', stream=False)
 i = 0
 for result in results:
     boxes = result.boxes  # Boxes object for bounding box outputs
-    masks = result.masks  # Masks object for segmentation masks outputs
-    keypoints = result.keypoints  # Keypoints object for pose outputs
-    probs = result.probs  # Probs object for classification outputs
-    obb = result.obb  # Oriented boxes object for OBB outputs
     # result.show()  # display to screen
     result.save(filename=f"result/result{i}.jpg")  # save to disk
     
