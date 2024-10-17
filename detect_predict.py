d08216ea17be46bf583d970bdb1163127a5003dd
candi4 <hojun327@naver.com>
2024-10-17 14:29:27 +0900

    Revised detect code

--- detect_predict.py
+++ detect_predict.py
@@ -1,17 +1,20 @@
 from ultralytics import YOLO
+import os
+from Utils.utils import root_dir
 
 # Load a model
-model = YOLO("/home/hojun/project/YOLO/runs/detect/train/weights/best.pt")  # pretrained model
+model = YOLO("detect.pt")  # pretrained model
 
 # Run batched inference on a list of images
-results = model('/home/hojun/project/YOLO/ezgif-6-13eb75ace9-jpg', stream=False)  # return a generator of Results objects
+results = model(f'{root_dir}/ezgif-6-13eb75ace9-jpg', stream=True)  # return a generator of Results objects
 
 # Process results generator
 i = 0
 for result in results:
     boxes = result.boxes  # Boxes object for bounding box outputs
     # result.show()  # display to screen
-    result.save(filename=f"result/result{i}.jpg")  # save to disk
+    os.makedirs("_result/detect", exist_ok=True)
+    result.save(filename=f"_result/detect/result{i}.jpg")  # save to disk
     
     
     print(i)
--- detect_train.py
+++ detect_train.py
@@ -7,6 +7,6 @@ model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
 # n s m l x
 
 # Train the model with GPUs
-results = model.train(data=f"{root_dir}/Finger detection via sementation.v5i.yolov8/data.yaml", epochs=10000, imgsz=640, device=[0,],
+results = model.train(data=f"{root_dir}/Finger detection via sementation.v5i.yolov8/data.yaml", epochs=10000, imgsz=640, device=None,
                       save_period=100)
 
