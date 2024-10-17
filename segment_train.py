d58e1af1ac544e0d4dbb19e36d092d46552833e2
candi4 <hojun327@naver.com>
2024-10-17 14:29:56 +0900

    Added segment code

---
+++ segment_train.py
@@ -0,0 +1,12 @@
+from ultralytics import YOLO
+from Utils.utils import root_dir
+
+
+# Load a model
+model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
+# n s m l x
+
+# Train the model with GPUs
+results = model.train(data=f"{root_dir}/Fingertip_segmentation.v1i.yolov8/data.yaml", epochs=10000, imgsz=640, device=None,
+                      save_period=100)
+
