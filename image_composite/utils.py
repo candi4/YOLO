89aa020b9c0a6a6ef668cb09cf9f89fff2a32b33
candi4 <hojun327@naver.com>
2024-11-14 21:18:15 +0900

    Added a class generating dataset

--- image_composite/utils.py
+++ image_composite/utils.py
@@ -1,6 +1,20 @@
 import cv2
 import yaml
 import numpy as np
+import os
+
+
+def draw_bbox(img, bbox, color, thickness:int):
+    """
+    Parameters
+    ----------
+    img
+        d
+    bbox: int array
+        [[x1 y1] [x2 y2]]
+    """
+    drawed_img = cv2.rectangle(img.copy(), pt1=bbox[0], pt2=bbox[1], color=color, thickness=thickness)
+    return drawed_img
 
 
 def crop_nonzero(arr):
@@ -82,59 +96,101 @@ def overlay_image(background, bg_scale, object_img, x, y, obj_scale=1.0, angle=0
 
 
 
-def generate_yolo_label(yaml_path, img_width, img_height, x_center, y_center, obj_width, obj_height, class_id=0):
-    # Calculate YOLO annotation format values
-    x_center_norm = x_center / img_width
-    y_center_norm = y_center / img_height
-    width_norm = obj_width / img_width
-    height_norm = obj_height / img_height
+def create_yolo_label(image, bbox, image_filename, label_filename, class_id=0):
+    """Saves image and YOLO formatted label file for YOLO training.
 
-    # YAML data
-    data = {
-        'path': yaml_path,
-        'x_center': x_center_norm,
-        'y_center': y_center_norm,
-        'width': width_norm,
-        'height': height_norm,
-        'class': class_id
-    }
-    return data
+    Parameters
+    ----------
+    - image: The image array.
+    - bbox: Bounding box in (x_min, y_min, x_max, y_max) format.
+    - image_filename: File path to save the image.
+    - label_filename: File path to save the label file.
+    - class_id: Integer class ID for the object (default is 0).
+    """
+    # Save image
+    cv2.imwrite(image_filename, image)
+    
+    # Get image dimensions
+    img_height, img_width = image.shape[:2]
+    
+    # Calculate YOLO format bounding box coordinates
+    [x_min, y_min], [x_max, y_max] = bbox
+    x_center = (x_min + x_max) / 2 / img_width
+    y_center = (y_min + y_max) / 2 / img_height
+    width = (x_max - x_min) / img_width
+    height = (y_max - y_min) / img_height
+    
+    # Write YOLO label to file
+    with open(label_filename, 'w') as label_file:
+        label_file.write(f"{class_id} {x_center} {y_center} {width} {height}\n")
+
+
+class GenYoloData():
+    def __init__(self, bg_filename, obj_filename,
+                 ):
+        # Load background and object images
+        self.bg_filename = bg_filename
+        self.obj_filename = obj_filename
+        self.background = cv2.imread(bg_filename, cv2.IMREAD_GRAYSCALE)
+        self.object_img = cv2.imread(obj_filename, cv2.IMREAD_GRAYSCALE)
+        assert self.background is not None, f"Wrong filename. bg_filename={bg_filename}"
+        assert self.object_img is not None, f"Wrong filename. obj_filename={obj_filename}"
+        self.bbox = None
+        self.combined_img = None
+
+    def combine(self, bg_scale, obj_scale, obj_range,
+                bg_weight, obj_weight, gamma,
+                regionimg_filename=None):
+        obj_range = np.array(obj_range, dtype=int)
+
+        # Calculate variables
+        obj_origin_range = obj_range.astype(float)
+        obj_origin_range[1] -= np.linalg.norm(self.object_img.shape[:2]) * (obj_scale/bg_scale)
+        obj_origin_range = obj_origin_range.astype(int)
+        angle = np.random.uniform(0,360)
+        x = int(np.random.uniform(*obj_origin_range[:,0]) * bg_scale)
+        y = int(np.random.uniform(*obj_origin_range[:,1]) * bg_scale)
+        obj_origin = np.array([x,y], dtype=int)
+
+        # Combine images
+        combined_img, background, object_img = overlay_image(background=self.background, bg_scale=bg_scale, 
+                                                             object_img=self.object_img, obj_scale=obj_scale, x=x, y=y, angle=angle,
+                                                             bg_weight=bg_weight, obj_weight=obj_weight, gamma=gamma,
+                                                             )
+        self.combined_img = combined_img
+
+        # Calculate bounding box
+        self.bbox = np.zeros((2,2)).astype(int) # [[x1 y1] [x2 y2]]
+        self.bbox[0] = obj_origin
+        self.bbox[1] = obj_origin + np.array(object_img.shape[:2][::-1])
+
+        if isinstance(regionimg_filename, str):
+            # Verify the range
+            bg_range_img = draw_bbox(background, bbox=obj_range, color=50, thickness=2)
+            bg_range_img = draw_bbox(bg_range_img, bbox=obj_origin_range, color=200, thickness=2)
+            cv2.imwrite(regionimg_filename, bg_range_img)
+        return self.combined_img
+    def save(self, directory, dataname, class_id=0):
+        image_filename = directory + '/images/' + dataname + '.jpg'
+        label_filename = directory + '/labels/' + dataname + '.txt'
+        assert not os.path.exists(image_filename), f"The file already exists: {image_filename}"
+        assert not os.path.exists(label_filename), f"The file already exists: {label_filename}"
+        os.makedirs(os.path.dirname(image_filename), exist_ok=True)
+        os.makedirs(os.path.dirname(label_filename), exist_ok=True)
+        create_yolo_label(image=self.combined_img, bbox=self.bbox, image_filename=image_filename, label_filename=label_filename, class_id=class_id)
+        self.bbox = None
+        self.combined_img = None
+
+
 
 if __name__ == "__main__":
-    # Load background and object images
-    bg_filename = 'ddca3f92-4b8e-4672-bb6b-f3594ad4e304.jpg'
-    obj_filename = '(ry0)(rx0)(delx0.1).png'
-    background = cv2.imread(f"background/{bg_filename}", cv2.IMREAD_GRAYSCALE)
-    object_img = cv2.imread(f"object/{obj_filename}", cv2.IMREAD_GRAYSCALE)
-
-    # Set numbers
-    bg_scale=8.0
-    obj_scale=0.8
-    angle=np.random.uniform(0,360)
-    obj_range = np.array([[120,120],  # x1 y1
-                          [470,450]]) # x2 y2
-    obj_origin_range = obj_range.astype(float)
-    obj_origin_range[1] -= np.linalg.norm(object_img.shape[:2]) * (obj_scale/bg_scale)
-    obj_origin_range = obj_origin_range.astype(int)
-    x = int(np.random.uniform(*obj_origin_range[:,0]) * bg_scale)
-    y = int(np.random.uniform(*obj_origin_range[:,1]) * bg_scale)
-    # Verify the range
-    bg_range_img = cv2.rectangle(background.copy(), pt1=obj_range[0], pt2=obj_range[1], color=50, thickness=2)
-    bg_range_img = cv2.rectangle(bg_range_img, pt1=obj_origin_range[0], pt2=obj_origin_range[1], color=200, thickness=2)
-    cv2.imwrite(f'bg_range/{bg_filename}',bg_range_img)
-
-    # Combine images
-    combined_img, background, object_img = overlay_image(background=background, bg_scale=bg_scale, object_img=object_img, obj_scale=obj_scale, x=x, y=y, angle=angle,
-                                                         bg_weight=0.6, obj_weight=1.3, gamma=0.,
-                                                         )
-    
-    cv2.imwrite('combined_img.jpg',combined_img)
-
-    # # YAML label generation
-    # yaml_data = generate_yolo_label(combined_img, bbox=)
-
-    # # Save YAML
-    # with open("yolo_annotation.yaml", "w") as file:
-    #     yaml.dump(yaml_data, file)
+    genyolo = GenYoloData(bg_filename='raw_images/background/ddca3f92-4b8e-4672-bb6b-f3594ad4e304.jpg',
+                          obj_filename='raw_images/object/(ry0)(rx20)(delx0.1).png')
+    for i in range(10):
+        genyolo.combine(bg_scale=8.0, obj_scale=0.8, obj_range=[[120,120],  # x1 y1
+                                                                [470,450]], # x2 y2
+                        bg_weight=0.6, obj_weight=1.3, gamma=0.,
+                        )
+        genyolo.save(directory='Dataset.yolo/unannotated/', dataname=f'data{i}')
 
     print("YAML file saved successfully!")
