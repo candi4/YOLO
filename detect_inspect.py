a6c07a8bd43957acb139c249f8a2056004b4669f
candi4 <hojun327@naver.com>
2024-11-14 21:25:01 +0900

    Translate to English annotations

--- detect_inspect.py
+++ detect_inspect.py
@@ -14,30 +14,26 @@ def load_yolo_labels(label_path):
     return yolo_bboxes
 
 def visualize_yolo_bounding_boxes(image_path, label_path):
-    # 이미지를 읽어옵니다.
     image = cv2.imread(image_path)
 
-    # YOLO 레이블을 로드합니다.
     yolo_bboxes = load_yolo_labels(label_path)
 
-    # 바운딩 박스를 그립니다.
+    # Draw bounding box
     height, width, _ = image.shape
     for bbox in yolo_bboxes:
         class_id, center_x, center_y, box_width, box_height = bbox
         
-        # 좌표 변환
         x1 = int((center_x - box_width / 2) * width)
         y1 = int((center_y - box_height / 2) * height)
         x2 = int((center_x + box_width / 2) * width)
         y2 = int((center_y + box_height / 2) * height)
         
-        # 바운딩 박스 그리기
         cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)  # 초록색 박스
         cv2.putText(image, f'Class {class_id}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
     cv2.imwrite('del.jpg',image)
 
 # 사용 예시
-image_path = 'image_composite/Dataset.yolo/unannotated/images/data1.jpg'  # 이미지 경로
-label_path = 'image_composite/Dataset.yolo/unannotated/labels/data1.txt'  # YOLO 레이블 파일 경로
+image_path = 'image_composite/Dataset.yolo/unannotated/images/data1.jpg'
+label_path = 'image_composite/Dataset.yolo/unannotated/labels/data1.txt'
 visualize_yolo_bounding_boxes(image_path, label_path)
