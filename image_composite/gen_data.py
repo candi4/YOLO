eb1d1e77c2504f6281d9690b50415d68efb775d8
candi4 <hojun327@naver.com>
2024-11-16 02:43:34 +0900

    Added tqdm to visualize progress

--- image_composite/gen_data.py
+++ image_composite/gen_data.py
@@ -2,6 +2,7 @@ import yaml
 import time
 import os
 import numpy as np
+from tqdm import tqdm
 
 from utils import GenYoloData
 
@@ -26,26 +27,30 @@ else:
 obj_dir = params['obj_dir']
 obj_filenames = [path for path in os.listdir(obj_dir) if os.path.isfile(f"{obj_dir}/{path}")]
 
-for data_order in range(10000):
-    bg_filename = np.random.choice(list(params['background'].keys()))
-    bg_config = params['background'][bg_filename]
-    print(bg_filename)
-    obj_filename = np.random.choice(obj_filenames)
-    print(obj_filename)
-    genyolo = GenYoloData(bg_filename=f'{bg_dir}/{bg_filename}',
-                        obj_filename=f'{obj_dir}/{obj_filename}')
-    for i in range(1):
-        start_individual = time.time()
-        dataname = f'data{data_order}'
-        genyolo.combine(bg_scale=bg_config['bg_scale'], obj_scale=bg_config['obj_scale'], obj_range=bg_config['obj_range'],
-                        bg_weight=bg_config['bg_weight'], obj_weight=bg_config['obj_weight'], gamma=bg_config['gamma'],
-                        )
-        crop_shape = np.random.randint(*bg_config['crop_shape_range'], size=2)
-        genyolo.crop(crop_shape=crop_shape)
-        genyolo.save(directory='Dataset.yolo/unannotated/', dataname=dataname)
-        print(f"{data_order}th data saved: {dataname}")
-        print("    Individual consumed time:", time.time()-start_individual)
-        data_order += 1
+
+data_num = 100_000
+with tqdm(total=data_num) as pbar:
+    data_order = 0
+    while data_order < data_num:
+        bg_filename = np.random.choice(list(params['background'].keys()))
+        bg_config = params['background'][bg_filename]
+        print(bg_filename)
+        obj_filename = np.random.choice(obj_filenames)
+        print(obj_filename)
+        genyolo = GenYoloData(bg_filename=f'{bg_dir}/{bg_filename}',
+                            obj_filename=f'{obj_dir}/{obj_filename}')
+        for i in range(10):
+            start_individual = time.time()
+            dataname = f'data{data_order}'
+            genyolo.combine(bg_scale=bg_config['bg_scale'], obj_scale=bg_config['obj_scale'], obj_range=bg_config['obj_range'],
+                            bg_weight=bg_config['bg_weight'], obj_weight=bg_config['obj_weight'], gamma=bg_config['gamma'],
+                            )
+            crop_shape = np.random.randint(*bg_config['crop_shape_range'], size=2)
+            genyolo.crop(crop_shape=crop_shape)
+            genyolo.save(directory='Dataset.yolo/unannotated/', dataname=dataname)
+            print(f"{data_order}th data saved: {dataname}")
+            print("    Individual consumed time:", time.time()-start_individual)
+            data_order += 1; pbar.update(1)
 
 print("Generated dataset is saved successfully!")
 print("Total consumed time:", time.time()-start_main)
